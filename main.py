751e38424acfc465188c6d6d93299d51db46e92f
Shubham Dhingra <dhingra.shubham38@gmail.com>
2020-09-25 23:02:29 +0530

    - Added feature to include logo with badge - Removed some redundancy

--- main.py
+++ main.py
@@ -1,65 +1,48 @@
 import flask
 import pybadges
-from data import get_cf, get_cc, get_at, get_yuki, get_top
+from data import get_info
+from flask import request
 
 app = flask.Flask(__name__)
 app.url_map.strict_slashes = False
+
+logos = {
+    'codeforces': 'https://lh3.googleusercontent.com/WsR_f03nbqW3qZjCZeXUYmnmhSWXo3hQhLX9hgl9QHydCgbXQi_VJeAwnmtuIgTHKdQ',
+    'codechef': 'https://i.pinimg.com/originals/c5/d9/fc/c5d9fc1e18bcf039f464c2ab6cfb3eb6.jpg',
+    'atcoder': 'https://img.atcoder.jp/assets/atcoder.png',
+    'topcoder': 'https://raw.githubusercontent.com/donnemartin/interactive-coding-challenges/master/images/logo_topcoder.png',
+    'yukicoder': 'https://pbs.twimg.com/profile_images/875757061669232640/T1_mPQuO_400x400.jpg'
+}
+
+website_text = {
+    'atcoder': 'AtCoder',
+    'codechef': 'Codechef',
+    'codeforces': 'Codeforces',
+    'topcoder': 'TopCoder',
+    'yukicoder': 'YukiCoder'
+}
+
+
+@app.route("/<website>/<handle>")
+def get_badge(handle, website):
+    q = None or request.args.get('logo')
+    display_logo = True if (q and q.lower() == 'true') else False
+    logo = logos[website]
+    x = get_info(handle, website)
+    rating, color = str(x[0]), str(x[1])
+    text = website_text[website.lower()]
+    if display_logo:
+        badge = pybadges.badge(left_text=text, right_text=rating, right_color=color, logo=logo, embed_logo=True)
+    else:
+        badge = pybadges.badge(left_text=text, right_text=rating, right_color=color)
+    response = flask.make_response(badge)
+    response.content_type = 'image/svg+xml'
+    return response
 
 
 @app.route("/")
 def home():
     return "This API is working."
-
-
-@app.route("/codeforces/<user>")
-def codeforces(user):
-    # logo='https://lh3.googleusercontent.com/WsR_f03nbqW3qZjCZeXUYmnmhSWXo3hQhLX9hgl9QHydCgbXQi_VJeAwnmtuIgTHKdQ'
-    x = get_cf(user)
-    rating, colour = x[0], x[1]
-    badge = pybadges.badge(left_text='Codeforces', right_text=rating, right_color=colour)
-    response = flask.make_response(badge)
-    response.content_type = 'image/svg+xml'
-    return response
-
-
-@app.route("/codechef/<user>")
-def codechef(user):
-    # logo='https://i.pinimg.com/originals/c5/d9/fc/c5d9fc1e18bcf039f464c2ab6cfb3eb6.jpg'
-    x = get_cc(user)
-    rating, colour = x[0], x[1]
-    badge = pybadges.badge(left_text='Codechef', right_text=rating, right_color=colour)
-    response = flask.make_response(badge)
-    response.content_type = 'image/svg+xml'
-    return response
-
-
-@app.route("/atcoder/<user>")
-def atcoder(user):
-    # logo='https://img.atcoder.jp/assets/atcoder.png'
-    x = get_at(user)
-    rating, colour = x[0], x[1]
-    badge = pybadges.badge(left_text='AtCoder', right_text=rating, right_color=colour)
-    response = flask.make_response(badge)
-    response.content_type = 'image/svg+xml'
-    return response
-
-@app.route("/topcoder/<user>")
-def topcoder(user):
-    x = get_top(user)
-    rating, color = x[0], x[1]
-    badge = pybadges.badge(left_text='TopCoder', right_text=str(rating), right_color=color)
-    response = flask.make_response(badge)
-    response.content_type = 'image/svg+xml'
-    return response
-
-@app.route("/yukicoder/<user>")
-def yukicoder(user):
-    x = get_yuki(user)
-    level, colour = x[0], x[1]
-    badge = pybadges.badge(left_text='YukiCoder', right_text=level, right_color=colour)
-    response = flask.make_response(badge)
-    response.content_type = 'image/svg+xml'
-    return response
 
 
 @app.errorhandler(404)
